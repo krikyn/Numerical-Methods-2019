f4514cd6e89657a7928e29c7cab2c72ed6bb1ff2
rofleksey <evilmorty@yandex.ru>
2019-05-22 01:10:25 +0300

    Changed generation, added conjugate gradient method, created tester

--- test.py
+++ test.py
@@ -3,21 +3,20 @@ import os
 import numpy as np
 
 from conjugate_gradient_method import conjugate_gradient
-from generate_matrices import DATA_DIR
+
+DATA_DIR = "data"
 
 
-def test_method(methodName, fun, A, b):
+def test_method(method_name, matrix_name, fun, A, b):
     x = fun(A=A, b=b)
-    print("%s: %s" % (methodName, x))
+    print('%s (%s)' % (method_name, matrix_name))
+    print(str(x))
 
 
-filenames = ['well_conditioned_matrix', 'random_matrix', 'poorly_conditioned_matrix']
-files = [os.path.join(DATA_DIR, name + ".npy") for name in filenames]
+filename_arr = ['well_conditioned_matrix', 'random_matrix', 'poorly_conditioned_matrix']
 b = np.load(os.path.join(DATA_DIR, "b.npy"))
 methods = [('conjugate_gradient', conjugate_gradient)]
-matrices = [np.load(x) for x in files]
+matrices = [(name, np.load(os.path.join(DATA_DIR, name + ".npy"))) for name in filename_arr]
 for method in methods:
-    for m in matrices:
-        n = m.shape[0]
-        zero = np.zeros(n)
-        test_method(method[0], method[1], m, zero)
+    for matrix in matrices:
+        test_method(method[0], matrix[0], method[1], matrix[1], b)
